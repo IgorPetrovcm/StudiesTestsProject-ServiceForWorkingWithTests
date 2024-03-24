bd45df0fda9342935911ea20e2b38e2f9d5c65ff
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-03-24 20:21:20 +0300

    Завершил базово весь функцилнал API

--- CollegeTestsProject_FastAPI/Persistence/testsRepository.py
+++ CollegeTestsProject_FastAPI/Persistence/testsRepository.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 import sys
-from bson import json_util
 import json
 sys.path.append("../Models")
 
@@ -11,7 +10,7 @@ class TestRepository:
     def __init__(self, context : MongoClient):
         self.__context : MongoClient = context
 
-    def __test_to_json_parse(test : Test) -> str:
+    def test_to_json_parse(self, test):
         questions_json = ""
         i = 1
         for question in test.questions:
@@ -22,13 +21,13 @@ class TestRepository:
             i += 1
 
         title_json = "\"title\": \"" + test.title + "\"" 
-
         return "{" + title_json + "," + "\"questions\":" + "[" + questions_json + "]" + "}"
+
 
     def create_test(self, test: Test):
         db = self.__context["tests"]
 
-        db["tests"].insert_one(json.loads(self.__test_to_json_parse(test)))
+        db["tests"].insert_one(json.loads(self.test_to_json_parse(test)))
 
     def get_titles(self):
         db = self.__context["tests"]
@@ -46,4 +45,16 @@ class TestRepository:
 
         for key in cursor:
             return key
+        
+    def update_test(self, title, _test : Test):
+        db = self.__context["tests"]
+
+        test_json = self.test_to_json_parse(_test)
+
+        db["tests"].replace_one(filter={"title":title}, replacement=json.loads(test_json))
+
+    def delete_test(self, title : str):
+        db = self.__context["tests"]
+
+        db["tests"].delete_one({"title": title})
         
--- CollegeTestsProject_FastAPI/main.py
+++ CollegeTestsProject_FastAPI/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Body
+from fastapi.responses import Response
 from fastapi.responses import JSONResponse
-import json
+from fastapi.encoders import jsonable_encoder
 import pydantic
 from pymongo import MongoClient
 
-from Models.question import Question
 from Models.test import Test
-from Environment.environment import Environment
+from Environment.environment import Environment 
 from Persistence.applicationContext import ApplicationContext
 from Persistence.testsRepository import TestRepository
 
@@ -20,23 +20,30 @@ def create_test(data: Test):
     try:
         repository.create_test(data)
     except pydantic.ValidationError as ex:
-        print(ex.errors())
+        return Response(status_code=400)
 
 @webApp.get("/gettitles/")
 def get_titles():
     titles = repository.get_titles()
-    response_json = json.dumps(titles, default=lambda o: o.__dict__)
-    return JSONResponse(response_json)
+    json_data = jsonable_encoder(titles)
+    return JSONResponse(content=json_data)
 
 @webApp.get("/gettest/{title}")
 def get_test(title: str):
     response_dict = repository.get_test(title)
-    print (response_dict)
     response_dict.pop("_id")
-    json_response = json.dumps(response_dict)
-
-    return JSONResponse(json_response )
+    json_data = jsonable_encoder(response_dict)
+    return JSONResponse(json_data)
 
 @webApp.put("/updatetest/{title}")
-def update_test(title : str):
-    
+def update_test(data : Test, title : str):
+    try:
+        repository.update_test(title=title, _test=data)
+        return Response(status_code=200)
+    except pydantic.ValidationError as ex:
+        return Response(status_code=400)
+
+@webApp.delete("/deletetest/{title}")
+def delete_test(title : str):
+    repository.delete_test(title)
+    return Response(status_code=200)
